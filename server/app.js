5b9c9956ce5a100c5d39c3d5c2efe8790252c36e
ngirmes <n.girmes@yahoo.com>
2026-02-16 15:42:04 -0800

    json parser

--- server/app.js
+++ server/app.js
@@ -9,6 +9,35 @@ const myLogger = function(req, res, next){
     next()
 }
 
+//json parsing middleware (could just use express.json() but wanted to show understanding)
+const jsonParser = (req, res, next) => {
+    // only run for requests that SHOULD have a body
+    if (req.method === 'POST' || req.method === 'PUT') {
+        let data = ''
+
+        req.on('data', chunk => {
+            data += chunk
+        })
+
+        req.on('end', () => {
+            if (!data) {
+                req.body = {}
+                return next()
+            }
+
+            try {
+                req.body = JSON.parse(data)
+                next()
+            } catch (err) {
+                res.status(400).json({ error: 'Invalid JSON' })
+            }
+        })
+    } else {
+        next()
+    }
+}
+
+
 app.use(myLogger)
 
 app.get('/', (req, res) => {
@@ -19,7 +48,7 @@ app.get('/jobs', (req, res) =>{
     res.send(placeholder_data)
 })
 
-app.post('/jobs', (req, res) => {
+app.post('/jobs', jsonParser, (req, res) => {
     // parse user data
     // add to database
     // send confirmation to user
